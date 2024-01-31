612b7a8442b7a01fce3682ebfb944e10ec0eefe8
eestet75 <jo-se-oh@hotmail.com>
2024-01-31 19:41:54 +0100

    requested functions

--- R-scripts/demofunk.R
+++ R-scripts/demofunk.R
@@ -22,6 +22,16 @@ library(stringi)
 library(ggplot2)
 library(ggthemes)
 
+hent_register_info <- function(register){
+  register = toupper(register)
+  try <- tryCatch(defaults <- danstat::get_table_metadata(register)$variables,
+                  error = function(e) stop(paste0("The registry ", register, " could not be fetched. ")))
+  varnames <- defaults$id
+  values <- defaults$values
+  names(values) <- varnames
+  values
+}
+
 # hent data fra statistikbanken.dk
 # ---------------------------------------------------------------------
 
@@ -38,7 +48,8 @@ library(ggthemes)
 ##' @author Thomas A. Gerds <tag@@biostat.ku.dk>
 hent_data <- function(register,...){
     register = toupper(register)
-    defaults <- danstat::get_table_metadata(register)$variables
+    try <- tryCatch(defaults <- danstat::get_table_metadata(register)$variables,
+                    error = function(e) stop(paste0("The registry ", register, " could not be fetched. ")))
     varnames <- tolower(defaults$id)
     values <- defaults$values
     names(values) <- varnames
@@ -64,10 +75,23 @@ hent_data <- function(register,...){
             list(code = requested_args[[u]],
                  values = values[[ua]]$id)
         }else{
+            ## handle 99- problem and e.g., the problem with men 
             if (length(not_value <- setdiff(user_args[[ua]],values[[ua]]$id))){
-                if (not_value == "99" && ("99-" %in% values[[ua]]$id)){
-                    list(code = requested_args[[u]],
-                         values = sub("99","99-",user_args[[ua]]))
+                match_text_with_id <- match(tolower(not_value), tolower(values[[ua]]$text))
+                if (any(!is.na(match_text_with_id))){
+                  ## find which indeces of user_args[[ua]] are in a, ignoring case
+                  match_not_value_user_args <- na.omit(match(tolower(user_args[[ua]]),intersect(tolower(not_value), tolower(values[[ua]]$text))))
+                  user_args[[ua]][match_not_value_user_args] <- values[[ua]]$id[na.omit(match_text_with_id)]
+                  ## remove match_not_value_user_args from not_value
+                  not_value <- not_value[-match_not_value_user_args]
+                  if (length(not_value) > 0){
+                    warning("removing ", paste0(not_value, collapse = ", ")," from ", ua, " because it/they is/are not valid value(s)")
+                    ## remove not_value from user_args[[ua]]
+                    user_args[[ua]] <- user_args[[ua]][!user_args[[ua]] %in% not_value]
+                  }
+                } 
+                else if (length(not_value) == 1 && not_value == "99" && ("99-" %in% values[[ua]]$id)){
+                  user_args[[ua]] <- sub("99","99-",user_args[[ua]])
                 } else{
                     # fix 99- problem of register dod
                     list(code = "error",
@@ -75,9 +99,8 @@ hent_data <- function(register,...){
                          problems = paste0(not_value,collapse = ","),
                          allowed = paste0(values[[ua]]$id,collapse = ","))
                 }
-            } else{
-                list(code = requested_args[[u]],values = user_args[[ua]])
-            }
+            } 
+            list(code = requested_args[[u]],values = user_args[[ua]])
         }})
     has_problems = any(sapply(vars,"[[","code") == "error")
     if (has_problems){
